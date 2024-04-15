8239a9592f617523357e8ec60ac9796911c4d847
shahrukhk01 <khanshahrukhbsc@gmail.com>
2024-04-15 19:40:24 +0530

    to achieve constructor overloading

--- oop/Cuboid.py
+++ oop/Cuboid.py
@@ -1,18 +1,26 @@
 class Cuboid:
-    def __init__(self,l,b,h):
+    # we can define defalut value to constructor to achieve constructor overloading.
+    def __init__(self,l=0,b=0,h=0):
+        print(id(self));
         #inside constructor declaration and initialization will be variable
         self.length = l;
         self.breadth = b;
         self.height = h;
 
+# we can not declare more then one constructor
+#    def __init__(self,l,b):
+#        pass
+
+
     def leadArea(self):
-        #self keyword must be passed because of they represent declaration of variable.
+        #self keyword must be passed because of they represent declaration of variable.__
         return self.length*self.breadth;
 
     def total(self):
         return 2*(self.length*self.breadth + self.breadth*self.height + self.length*self.height);
 
     def volume(self):
+        print(id(self));
         return self.length*self.breadth*self.height;
 
 
@@ -20,3 +28,8 @@ cuboid = Cuboid(2,3,4);
 print(cuboid.leadArea());
 print(cuboid.total());
 print(cuboid.volume());
+print(id(cuboid));
+
+c2 = Cuboid(1,4);
+print(id(c2));
+c2.volume();
