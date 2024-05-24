bb22ae60cea4aa1d081e179a5cf307a03a5e8b61
Shahrukh Khan <93510831+shahrukh010@users.noreply.github.com>
2024-05-24 11:51:44 +0000

    filtering array

--- numpy/numpyfunction.py
+++ numpy/numpyfunction.py
@@ -35,3 +35,34 @@ result_even = numpy.where(random_data%2==0)
 print(result_even);
 result_odd = numpy.where(random_data %2!=0)
 print(result_odd);
+
+#search data from sorted array
+sorted_data = numpy.array([1,2,3,4,5,7,8,9,10])
+#it is binarysearch
+result = numpy.searchsorted(sorted_data,8)
+print(result);
+print(numpy.searchsorted(sorted_data,100));
+#search more then one element.
+result = numpy.searchsorted(sorted_data,[4,8,1])
+print(result);
+
+#createing filter
+
+data = numpy.array([10,15,17,9,8,11,20])
+result =filter(lambda num: num%2==0,data);
+for item in result:
+  print(item,end=',');
+print(result);
+
+result = list(filter(lambda num : num%2 !=0,data))
+print(result);
+
+#createing filtering array
+
+input = numpy.array([99, 88, 77, 66, 55])
+filter_result = input>90;
+print(filter_result);
+print(input[filter_result]);
+filter_result = input<90;
+result = input[filter_result];
+print(result);
